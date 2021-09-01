c0a5ae7908376438f45764d4a93ab8475fc4b1f9
trinhvanminh <minh.a1.no.0@gmail.com>
2021-09-02 05:20:28 +0700

    Form default behavior

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+// set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //	HTTP logger
@@ -20,8 +21,18 @@ app.engine(
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
+// routing
 app.get("/", function (req, res) {
 	res.render("home");
+});
+
+app.get("/news", function (req, res) {
+	console.log(req.query.q)
+	res.render("news");
+});
+
+app.get("/search", function (req, res) {
+	res.render("search");
 });
 
 app.listen(port, () =>
