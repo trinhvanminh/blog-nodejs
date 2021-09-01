62a12ba92041a6631833a46a27cac05d6e9f4fc8
trinhvanminh <minh.a1.no.0@gmail.com>
2021-09-02 04:33:01 +0700

    add bootstrap 4

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # blog-nodejs
+
+
+1. Node
+2. [express](https://www.npmjs.com/package/express)
+3. [nodemon](https://www.npmjs.com/package/nodemon) --inspect src/index.js
+4. [morgan](https://www.npmjs.com/package/morgan) (http logger)
+5. [express-handlebars](https://www.npmjs.com/package/express-handlebars): template-engine
+6. [node-sass](https://www.npmjs.com/package/node-sass)
+   - `node-sass --watch src/resources/scss --output src/public/css`  
+7. add bootstrap 4
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+const path = require("path");
 const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+//	HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(
+	".hbs",
+	exphbs({
+		extname: ".hbs",
+	})
+);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "/resources/views"));
+
 app.get("/", function (req, res) {
-	res.send("Hello World");
+	res.render("home");
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+	console.log(`Example app listening at http://localhost:${port}`)
+);
