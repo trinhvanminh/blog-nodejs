fac76b76597337191e53b1e77f0cc06559e521b4
trinhvanminh <minh.a1.no.0@gmail.com>
2021-09-21 10:42:46 +0700

    trash can / restore / force delete

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -20,10 +20,9 @@ class CoursesController {
 
 	// [POST] /course/store
 	store(req, res, next) {
-		const formData = req.body;
-		formData.image = `https://img.youtube.com/vi/${formData.videoId}/sddefault.jpg`;
-		const course = new Course(formData);
-		course.save().then(res.redirect("/")).catch(next);
+		req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+		const course = new Course(req.body);
+		course.save().then(res.redirect("/me/stored/courses")).catch(next);
 	}
 
 	// [GET] /course/:id/edit
@@ -41,15 +40,29 @@ class CoursesController {
 	update(req, res, next) {
 		const formData = req.body;
 		formData.image = `https://img.youtube.com/vi/${formData.videoId}/sddefault.jpg`;
-		
+
 		Course.findByIdAndUpdate(req.params.id, formData)
 			.then(() => res.redirect("/me/stored/courses"))
 			.catch(next);
 	}
 
+	// [PATCH] /courses/:id/restore
+	restore(req, res, next) {
+		Course.restore({ _id: req.params.id })
+			.then(() => res.redirect("/me/trash/courses"))
+			.catch(next);
+	}
+
 	// [DELETE] /courses/:id
 	delete(req, res, next) {
-		Course.findByIdAndDelete(req.params.id)
+		Course.deleteById(req.params.id)
+			.then(() => res.redirect("back"))
+			.catch(next);
+	}
+
+	// [DELETE] /courses/:id/force
+	forceDelete(req, res, next) {
+		Course.deleteOne({ _id: req.params.id })
 			.then(() => res.redirect("back"))
 			.catch(next);
 	}
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
-const slug = require('mongoose-slug-generator'); //generator name to slug
-
-mongoose.plugin(slug);
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator"); //generator name to slug
+const mongooseDelete = require("mongoose-delete"); //sort delete
 
 const Schema = mongoose.Schema;
+const Course_schema = new Schema(
+	{
+		name: { type: String, require: true },
+		description: { type: String, maxLength: 600 },
+		image: { type: String },
+		videoId: { type: String, require: true },
+		slug: { type: String, slug: "name", unique: true },
+	},
+	{ timestamps: true }
+);
 
+// add plugin
+mongoose.plugin(slug);
+mongoose.plugin(mongooseDelete, { deletedAt: true, overrideMethods: "all" });
 
-const Course_schema = new Schema({
-	name: { type: String, require: true },
-	description: { type: String, maxLength: 600 },
-	image: { type: String },
-	videoId: { type: String, require: true},
-	slug: { type: String, slug: 'name', unique: true },
-}, { timestamps: true, });
-
-module.exports = mongoose.model('Course', Course_schema);
+module.exports = mongoose.model("Course", Course_schema);
